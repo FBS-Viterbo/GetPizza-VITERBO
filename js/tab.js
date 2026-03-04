d8fd42d4c6fb10bd15bed6fe2d8738803c46eda7
nItsu69 <alisonmaliksi07@gmail.com>
2026-03-04 15:06:59 +0800

    tag

--- js/tab.js
+++ js/tab.js
@@ -5,5 +5,18 @@ foodItems.forEach((item) => {
     foodItems.forEach((i) => i.classList.remove("active"));
 
     item.classList.add("active");
+    console.log(item.id);
+
+    showCards(item.id);
   });
 });
+
+function showCards(id) {
+  // Remove 'active' from all cards
+  const allCards = document.querySelectorAll(".foodCard");
+  allCards.forEach((card) => card.classList.remove("active"));
+
+  // Add 'active' to cards that match the clicked li's id
+  const targetCards = document.querySelectorAll(`.card_${id}`);
+  targetCards.forEach((card) => card.classList.add("active"));
+}
