782a26a798b65598dd6aedc61435aa7e9e9454bc
nItsu69 <alisonmaliksi07@gmail.com>
2026-03-05 15:28:26 +0800

    update

--- js/tab.js
+++ js/tab.js
@@ -1,4 +1,5 @@
 const foodItems = document.querySelectorAll(".foodChoices li");
+const menuItems = document.querySelectorAll(".menuChoices li");
 
 foodItems.forEach((item) => {
   item.addEventListener("click", () => {
@@ -14,6 +15,27 @@ foodItems.forEach((item) => {
 function showCards(id) {
   // Remove 'active' from all cards
   const allCards = document.querySelectorAll(".foodCard");
+  allCards.forEach((card) => card.classList.remove("active"));
+
+  // Add 'active' to cards that match the clicked li's id
+  const targetCards = document.querySelectorAll(`.card_${id}`);
+  targetCards.forEach((card) => card.classList.add("active"));
+}
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuItems.forEach((i) => i.classList.remove("active"));
+
+    item.classList.add("active");
+    console.log(item.id);
+
+    showMenuCards(item.id);
+  });
+});
+
+function showMenuCards(id) {
+  // Remove 'active' from all cards
+  const allCards = document.querySelectorAll(".menuCard");
   allCards.forEach((card) => card.classList.remove("active"));
 
   // Add 'active' to cards that match the clicked li's id
